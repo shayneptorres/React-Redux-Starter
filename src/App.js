991fae6256aa1c2a9faa453dfad5ac71a882917e
Shayne Torres <storres@nationalmerchants.com>
2017-03-10 12:30:38 -0800

    Add home path with react router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Router, Route, Link, IndexRoute, hashHistory, browserHistory} from "react-router";
 
 import PlayerInfo from "./components/player_info";
 
@@ -8,11 +8,13 @@ import PlayerInfo from "./components/player_info";
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <PlayerInfo />
-      </div>
+      <Router history={hashHistory}>
+        <Route path="/" component={Home} />
+      </Router>
     );
   }
 }
+
+const Home = () => <div className="App"><PlayerInfo /></div>
 
 export default App;
